5f4bc41b5c683ff802f27e0ed0642fd45ea7fbfb
Giovanny Dani Saputra <47962353+L200170159@users.noreply.github.com>
2019-03-27 23:30:18 +0700

    Add files via upload

---
+++ Modul 4/7.py
@@ -0,0 +1,51 @@
+def binSe(kumpulan, target):
+
+    temp = []
+
+    low = 0
+
+    high = len(kumpulan)-1
+
+    while low <= high :
+
+        mid = (high+low)//2
+
+        if kumpulan[mid] == target:
+
+            midKiri = mid-1
+
+            while kumpulan[midKiri] == target:
+
+                temp.append(midKiri)
+
+                midKiri = midKiri-1
+
+            temp.append(mid)
+
+            midKanan = mid+1
+
+            while kumpulan[midKanan] == target:
+
+                temp.append(midKanan)
+
+                midKanan = midKanan+1
+
+            return temp
+
+        elif target < kumpulan[mid]:
+
+            high = mid-1
+
+        else:
+
+            low = mid+1
+
+    return False
+
+
+
+kumpulan = [14,4,19,99,17,8,19,45]
+
+target = 99
+
+print(binSe(kumpulan,target))
